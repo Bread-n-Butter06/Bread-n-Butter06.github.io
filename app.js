c9b3f3a252038ac45a9151198e80dbca9e2e2943
belaw888 <76421215+belaw888@users.noreply.github.com>
2022-03-26 23:02:22 -0400

    initialized data variables

--- app.js
+++ app.js
@@ -1,5 +1,62 @@
 'use strict';
 
+/**
+ * Data Points Order (Based on 422 Spreadsheet; Subject to change):
+ * 1.  Team # - int
+ * 2.  Match # - int
+ * 3.  Taxi? - boolean
+ * 4.  Auto Lower Hub - int
+ * 5.  Auto Upper Hub - int
+ * 6.  Teleop Lower Hub - int
+ * 7.  Teleop Upper Hub - int
+ * 8.  Attempted Climb? - boolean
+ * 9.  Climb level - String
+ * 10. Disconnected % - double
+ * 11. Played Defense % - double 
+ * 
+ * ----- End of 422 Data Columns
+ * 
+ * 12. Was Defended? - String (ie. "not defended" "minimally obstructed")
+ * 13. Played Defense Rating - String (ie. "effective" "not effective")
+ * 14. Comments - String 
+ * 15. No Show - boolean
+ * 16. Scout Initials - String
+ * 17. Data Entry Id - Date
+ */
+
+let allData = []; 
+  let teamNum = 0;
+  let matchNum = 0;
+  let taxi = false;
+  let autoLowHub = 0;
+  let autoUpHub = 0;
+  let teleLowHub = 0;
+  let teleUpHub = 0;
+  let climbAttempt = false;
+  let climbLevel = "";
+  let disconnected = 0;
+  let playedDefense = 0;
+
+  let wasDefended = "";
+  let playedDefenseRating = "";
+  let comments = "";
+  let noShow = false;
+  let scoutInitials = "";
+  let dataId = Date;
+  
+let submitForm = function(allData) { //unfinished function
+  allData.preventDefault()
+
+  let allData = 
+  [
+    teamNum, matchNum, taxi, autoLowHub, autoUpHub, /*Pre-Match and Auto*/
+    teleLowHub, teleUpHub, climbAttempt, climbLevel, /*Teleop and End*/
+    disconnected, playedDefense, /*Post-Match*/
+    wasDefended, playedDefenseRating, comments, noShow, scoutInitials, dataId.now() /*Data not in 422 sheet*/
+  ]
+
+}
+
 const allScreensDropdown = document.getElementById("allScreensDropdown");
 
 /* When the user clicks on the button,
